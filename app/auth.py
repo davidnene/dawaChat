71a154668ef3d974955737d8847803b334f589b6
David Nene <64870164+davidnene@users.noreply.github.com>
2024-11-02 23:33:04 +0300

    Setup auth

---
+++ app/auth.py
@@ -0,0 +1,29 @@
+from fastapi import Depends, HTTPException, status
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from models import Doctor
+
+SECRET_KEY = "aiplanettask1234"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def authenticate_user(email: str, password: str, db_session):
+    doctor = db_session.query(Doctor).filter(Doctor.email == email).first()
+    if doctor and verify_password(password, doctor.hashed_password):
+        return doctor
+    return None
